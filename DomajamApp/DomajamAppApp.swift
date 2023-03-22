cb7a35c31ca04f23c0311011e366f19434bf77e4
tussi5969 <sakura.0304.0226@gmail.com>
2023-03-23 01:44:43 +0900

    Initial Commit

---
+++ DomajamApp/DomajamAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomajamAppApp.swift
+//  DomajamApp
+//
+//  Created by 宮地篤士 on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DomajamAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
