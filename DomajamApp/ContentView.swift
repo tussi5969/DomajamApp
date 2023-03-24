3ea5d6ce193e40f221dd064edbcf7b6ee3ff7140
tussi5969 <sakura.0304.0226@gmail.com>
2023-03-24 22:27:16 +0900

    とりあえずnfcタグ読み取り部分を実装

--- DomajamApp/ContentView.swift
+++ DomajamApp/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import CoreNFC
 
 struct ContentView: View {
+    @State var data = ""
+    @State var showWrite = false
+    let holder = "読み込んだ情報を表示"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 30){
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                    TextField("読み込んだ情報を表示", text: self.$data)
+                        .foregroundColor(self.data.isEmpty ? .gray : .black)
+                        .padding()
+                }.frame(height: UIScreen.main.bounds.height * 0.3)
+
+                //Read Button
+                NFCButton(data: self.$data)
+                    .frame(height: UIScreen.main.bounds.height * 0.05)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+
+                Spacer()
+            }.frame(width: UIScreen.main.bounds.width * 0.9, alignment: .center)
+            .navigationBarTitle("NFC Read/Write", displayMode: .large)
+            .padding(.top, 20)
         }
-        .padding()
     }
 }
 
---
+++ DomajamApp/NFCButton.swift
@@ -0,0 +1,74 @@
+//
+//  NFCButton.swift
+//  DomajamApp
+//
+//  Created by 宮地篤士 on 2023/03/24.
+//
+
+import SwiftUI
+import CoreNFC
+
+struct NFCButton : UIViewRepresentable {
+    @Binding var data : String
+    func makeUIView(context: UIViewRepresentableContext<NFCButton>) -> UIButton {
+        let button = UIButton()
+        button.setTitle("Read", for: .normal)
+        button.backgroundColor = UIColor.gray
+        button.addTarget(context.coordinator, action: #selector(context.coordinator.beginScan(_:)), for: .touchUpInside)
+        return button
+    }
+    func updateUIView(_ uiView: UIButton, context: UIViewRepresentableContext<NFCButton>) {
+
+    }
+    func makeCoordinator() -> NFCButton.Coordinator {
+        return Coordinator(data: $data)
+    }
+
+    class Coordinator : NSObject, NFCNDEFReaderSessionDelegate {
+        var session : NFCReaderSession?
+        @Binding var data : String
+
+        init(data: Binding<String>) {
+            _data = data
+        }
+
+        @objc func beginScan(_ sender: Any) {
+            guard NFCNDEFReaderSession.readingAvailable else {
+                print("このタグは読み込む事ができません")
+                return
+            }
+
+            session = NFCNDEFReaderSession(delegate: self, queue: .main, invalidateAfterFirstRead: true)
+            session?.alertMessage = "スキャンします。 iPhoneを近づけて下さい"
+            session?.begin()
+        }
+
+
+        func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+            if let readerError = error as? NFCReaderError{
+
+                if (readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead)
+                    && (readerError.code != .readerSessionInvalidationErrorUserCanceled) {
+                    print("読み込みできませんでした")
+                }
+            }
+
+            self.session = nil
+
+        }
+
+        func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+            guard
+                let nfcMess = messages.first,
+                let record = nfcMess.records.first,
+                record.typeNameFormat == .absoluteURI || record.typeNameFormat == .nfcWellKnown,
+                let payload = String(data: record.payload, encoding: .utf8)
+            else {
+                return
+            }
+
+            print(payload)
+            self.data = payload
+        }
+    }
+}
