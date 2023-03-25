ebf438112c64a580646b32748e94e770105480b2
yutailang0119 <yutailang0119@gmail.com>
2023-03-25 18:49:02 +0900

    Add NFCTagPayload

---
+++ DomajamApp/NFCTagPayload.swift
@@ -0,0 +1,25 @@
+//
+//  NFCTagPayload.swift
+//  DomajamApp
+//
+//  Created by Yutaro Muta on 2023/03/25.
+//
+
+import Foundation
+
+enum NFCTagPayload: String {
+    case foo = "foofoofoofoofoo"
+    case bar = "barbarbarbarbar"
+    case baz = "bazbazbazbazbaz"
+
+    var point: Int {
+        switch self {
+        case .foo:
+            return 30
+        case .bar:
+            return 50
+        case .baz:
+            return 100
+        }
+    }
+}
